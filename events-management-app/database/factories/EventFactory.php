a42572fa27d0e40afe03fe4771c56f1fccbbb61b
batata <contactobrite@gmail.com>
2026-04-03 19:59:00 +0000

    Seeding Data for the REST API

--- events-management-app/database/factories/EventFactory.php
+++ events-management-app/database/factories/EventFactory.php
@@ -16,11 +16,14 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
+        $start = fake()->dateTimeBetween('now', '+1 month');
+        $end = (clone $start)->modify('+2 hours');
+
         return [
             'name' => fake()->unique()->sentence(3),
-            'description' => fake()->text,
-            'start_time' => fake()->dateTimeBetween('now', '+1 month'),
-            'end_time' => fake()->dateTimeBetween('+1 month', '+2 months'),
+            'description' => fake()->text(),
+            'start_time' => $start,
+            'end_time' => $end,
         ];
     }
 }
